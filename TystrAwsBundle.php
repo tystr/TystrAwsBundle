742a6f4080765ede3e32e53dc3ca918de7cee44b
Tyler Stroud <tyler@tylerstroud.com>
2013-12-27 18:54:22 -0800

    Initial commit

---
+++ TystrAwsBundle.php
@@ -0,0 +1,11 @@
+<?php
+namespace Tystr\Bundle\AwsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Tyler Stroud <tyler@tylerstroud.com>
+ */
+class TystrAwsBundle extends Bundle
+{
+}
