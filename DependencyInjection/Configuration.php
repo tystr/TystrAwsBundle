2afdcc5e0a88d75cca1512cd90f4cfd66be617bc
Tyler Stroud <tyler@tylerstroud.com>
2013-12-29 11:05:33 -0800

    Removed unused use statement

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,7 +2,6 @@
 
 namespace Tystr\Bundle\AwsBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeParentInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
